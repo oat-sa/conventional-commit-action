1278e805dbe5105111e09ae6f6923cbc7a9353de
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2020-12-17 22:20:13 +0100

    feat: generate recommendation message

--- index.js
+++ index.js
@@ -2,42 +2,88 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const conventionalRecommendedBump = require('conventional-recommended-bump');
 const conventionalPresetConfig = require('@oat-sa/conventional-changelog-tao');
+const gitSemverTags = require('git-semver-tags');
+const semverParse = require('semver/functions/parse');
+const semverInc = require('semver/functions/inc');
+
 
 async function main() {
     const token = core.getInput('github_token');
     const context = github.context;
 
     const octokit = github.getOctokit(token);
-    console.log("CONTEXT", JSON.stringify(context));
+    const recommendation = await getVersion();
+    const lastTag = await getLastTag();
 
-    const versions = await getVersion();
+    let lastVersion;
+    let version;
+    if( lastTag && recommendation ){
+        const lastVersionObject = semverParse(lastTag);
+        lastVersion = lastVersionObject.version;
+        version = semverInc(lastVersionObject, recommendation.releaseType);
+    }
 
-    console.log('>>>VERSIONS<<<<', versions);
+    console.log('recommendation', recommendation);
+    console.log('lastTag', lastTag);
 
     await octokit.issues.createComment({
 		repo: context.repo.repo,
 		owner: context.repo.owner,
 		issue_number: context.payload.pull_request.number,
-		body: 'Hey 👋, this is Bertrand. I\'m testing some github action on this repo. No worry your PR is awesome !',
+		body: getMessage(recommendation, version)
 	})
 }
 
 
-async function getVersion() {
+async function getRecommandation(){
     return new Promise((resolve, reject) => {
         conventionalRecommendedBump(
             {
-                config: conventionalPresetConfig    //the preset cannot be used from string in an action due to missing lookups in node_modules
-            },
+                //the preset cannot be used from string in an action due to missing lookups in node_modules
+                config: conventionalPresetConfig                },
             {},
             (err, recommendation) => {
                 if (err) {
                     return reject(err);
                 }
 
-                resolve(recommendation);
+                resolve(recommendation, lastVersion, version);
             });
     });
+}
+
+async function getLastTag() {
+    return new Promise( (resolve, reject) => {
+        gitSemverTags((err, tags) => {
+            if (err) {
+                return reject(err);
+            }
+            if (tags && tags.length) {
+                return resolve(tags[0]);
+            }
+            return reject(new Error('no tag found'));
+        });
+    });
+}
+
+function getMessage({ stats, releaseType, reason } = recommendation, lastVersion, version) {
+    if(stats.commits > 0 && stats.commits === stats.unset){
+        return 'The commits are not compliant with the conventional commits format!';
+    }
+    let message = [];
+    if (stats.unset > 0 ) {
+        message.push(`${stats.unset} commits are not using the conventional commits formats. They will be ignored in version management.`);
+    }
+    message.push(
+        ` - last version: ${lastVersion}`
+    );
+    message.push(
+        ` - target version: **${version}**`
+    );
+    message.push(
+        reason
+    );
+    return message.join('\n');
 }
 
 main().catch(err => core.setFailed(err.message) );
