ed27b47b1ab149a6fe67acbe0337b5889b194df4
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2020-12-17 21:21:55 +0100

    feat: add recommended bump guessing

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const conventionalRecommendedBump = require('conventional-recommended-bump');
+const conventionalPresetConfig = require('@oat-sa/conventional-changelog-tao');
 
 async function main() {
     const token = core.getInput('github_token');
@@ -8,12 +10,40 @@ async function main() {
     const octokit = github.getOctokit(token);
     console.log("CONTEXT", JSON.stringify(context));
 
+    const versions = await getVersion();
+
+    console.log('>>>VERSIONS<<<<', versions);
+
     await octokit.issues.createComment({
 		repo: context.repo.repo,
 		owner: context.repo.owner,
 		issue_number: context.payload.pull_request.number,
 		body: 'Hey 👋, this is Bertrand. I\'m testing some github action on this repo. No worry your PR is awesome !',
 	})
+}
+
+
+async function getVersion() {
+    return new Promise((resolve, reject) => {
+        conventionalRecommendedBump(
+            {
+                preset: {
+                    name: '@oat-sa/tao'
+                }
+            },
+            {},
+            (err, recommendation) => {
+                if (err) {
+                    return reject(err);
+                }
+
+                const lastVersion = lastVersionObject.version;
+
+                //carefull inc mutate lastVersionObject
+                const version = semverInc(lastVersionObject, recommendation.releaseType);
+                resolve({ lastVersion, version, recommendation });
+            });
+    });
 }
 
 main().catch(err => core.setFailed(err.message) );
